81e04869cd5bbf330be6d3c14f1584916d6508f4
Gustavo <gusstavocta@gmail.com>
2024-12-03 00:13:23 -0500

    trabajo terminado

--- ProyectosJava/bookstore/bookstore/src/main/java/com/gusstavodev/bookstore/mainapp/MainApp.java
+++ ProyectosJava/bookstore/bookstore/src/main/java/com/gusstavodev/bookstore/mainapp/MainApp.java
@@ -1,4 +1,5 @@
 package com.gusstavodev.bookstore.mainapp;
+
 import com.gusstavodev.bookstore.model.Datos;
 import com.gusstavodev.bookstore.model.DatosLibro;
 import com.gusstavodev.bookstore.service.ConsumoAPI;
@@ -7,6 +8,7 @@ import com.gusstavodev.bookstore.service.ConvierteDatos;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 public class MainApp {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
@@ -14,55 +16,52 @@ public class MainApp {
     private final String URL_BASE = "https://gutendex.com/books/";
 
 
-
-   public void muestraLibro() {
-
-
-       var json = consumoApi.obtenerDatos(URL_BASE);
+    public void muestraLibro() {
 
 
-       var datos = conversor.obtenerDatos(json, Datos.class);
-       System.out.println(datos);
-
-       //Top 10 libros mas descargados
-       System.out.println("10 Libros mas descargados");
-       System.out.println("--------------------------------");
-       datos.resultados().stream()
-               .sorted(Comparator.comparing(DatosLibro::numeroDeDescargas).reversed())
-                       .limit(10)
-                       .map(l->l.titulo().toUpperCase())
-               .forEach(System.out::println);
+        var json = consumoApi.obtenerDatos(URL_BASE);
 
 
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+        //Top 10 libros mas descargados
+        System.out.println("10 Libros mas descargados");
+        System.out.println("--------------------------------");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibro::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
 
 
 //Buscar libro
-       System.out.println("Por favor escribe el nombre del  libro que deseas buscar:");
-         var nombreLibro = teclado.nextLine();
-            json = consumoApi.obtenerDatos(URL_BASE + "?search=" + nombreLibro.replace(" ", "+"));
-            Datos datosBusquedaLibro = conversor.obtenerDatos(json, Datos.class);
-       Optional<DatosLibro>libroBuscado = datosBusquedaLibro.resultados().stream()
-               .filter(l->l.titulo().toUpperCase().contains(nombreLibro.toUpperCase()))
-               .findFirst();
-         if(libroBuscado.isPresent()){
-                System.out.println("Libro encontrado: " + libroBuscado.get());
-         }
-            else{
-                System.out.println("Libro no encontrado");
-            }
+        System.out.println("Por favor escribe el nombre del  libro que deseas buscar:");
+        var nombreLibro = teclado.nextLine();
+        json = consumoApi.obtenerDatos(URL_BASE + "?search=" + nombreLibro.replace(" ", "+"));
+        Datos datosBusquedaLibro = conversor.obtenerDatos(json, Datos.class);
+        Optional<DatosLibro> libroBuscado = datosBusquedaLibro.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(nombreLibro.toUpperCase()))
+                .findFirst();
+        if (libroBuscado.isPresent()) {
+            System.out.println("Libro encontrado: " + libroBuscado.get());
+        } else {
+            System.out.println("Libro no encontrado");
+        }
 
 
-            //Generando estadisticas
-            System.out.println("Generando estadisticas de cantidad de descargas ==>");
-            var promedioDescargas = datos.resultados().stream()
-                    .mapToDouble(DatosLibro::numeroDeDescargas)
-                    .average()
-                    .orElse(0);
-            System.out.println("Promedio de descargas: " + promedioDescargas);
+        //Generando estadisticas
 
-   }
+        DoubleSummaryStatistics est = datos.resultados().stream()
+                .filter(d -> d.numeroDeDescargas() > 0)
+                .collect(Collectors.summarizingDouble(DatosLibro::numeroDeDescargas));
+        System.out.println("cantidad media de descargas: " + est.getAverage());
+        System.out.println("Maximo de descargas: " + est.getMax());
+        System.out.println("Minimo de descargas: " + est.getMin());
+        System.out.println("Cantidad de libros con descargas: " + est.getCount());
 
 
+    }
 
 
 }
